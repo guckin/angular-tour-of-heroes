38e7a97991c0b9883848153eb5d3e57c47f8d306
Stephen McGuckin <stephen.mcguckin@dat.com>
2019-11-14 20:01:50 -0800

    removes everything from the app component
    This corresponds to the "The Application Shell" part of the tutorial.
            see: https://angular.io/tutorial/toh-pt0#the-application-shell

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    await expect(page.getTitleText()).toEqual('Welcome to angular-tour-of-heroes!');
+    await expect(page.getTitleText()).toEqual('Tour of Heroes');
   });
 
   afterEach(async () => {
