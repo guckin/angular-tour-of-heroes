47c6894fc43ee6584cc6b0bf84e074c4af7bf930
Stephen McGuckin <stephen.mcguckin@dat.com>
2019-11-14 19:44:54 -0800

    await for things
    Not sure if this matter but going to do it anyway

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -8,9 +8,9 @@ describe('workspace-project App', () => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to angular-tour-of-heroes!');
+  it('should display welcome message', async () => {
+    await page.navigateTo();
+    await expect(page.getTitleText()).toEqual('Welcome to angular-tour-of-heroes!');
   });
 
   afterEach(async () => {
